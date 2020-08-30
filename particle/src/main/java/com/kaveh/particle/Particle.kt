4fad6c246105c9bac4c199cf8215d9d3cb653a48
kaveh <kaveh.fazaeli@gmail.com>
2020-08-30 21:36:37 +0430

    Init Particle Class and its properties

---
+++ particle/src/main/java/com/kaveh/particle/Particle.kt
@@ -0,0 +1,46 @@
+package com.kaveh.particle
+
+class Particle {
+
+    private lateinit var mPosition: Pair<Int, Int>
+    private lateinit var mVelocity: Pair<Float, Float>
+    val creationTime: Long = System.currentTimeMillis()
+    private var mOpacity = 1F
+    private var duration: Int = 0
+
+    init {
+    }
+
+    /**
+     * set position.
+     *
+     */
+    var position: Pair<Int, Int>
+        get() = mPosition
+        set(position) {
+            //TODO Validate Position
+            mPosition = position
+        }
+
+    /**
+     * set velocity of particle.
+     *
+     */
+    var velocity: Pair<Float, Float>
+        get() = mVelocity
+        set(velocity) {
+            //TODO Validate Velocity
+            mVelocity = velocity
+        }
+
+    /**
+     * set opacity of particle.
+     *
+     */
+    var opacity: Float
+        get() = mOpacity
+        set(opacity) {
+            if (opacity in 0.0..1.0)
+                mOpacity = opacity
+        }
+}
---
+++ particle/src/main/java/com/kaveh/particle/ParticleFactory.kt
@@ -0,0 +1,4 @@
+package com.kaveh.particle
+
+class ParticleFactory {
+}
