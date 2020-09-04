82ac35e4b0824e28d3cf8f2a6f6fe38bba8034ab
kaveh <kaveh.fazaeli@gmail.com>
2020-09-04 21:54:36 +0430

    ParticleFactory add auto-refresh particles state

--- particle/src/main/java/com/kaveh/particle/ParticleFactory.kt
+++ particle/src/main/java/com/kaveh/particle/ParticleFactory.kt
@@ -7,7 +7,7 @@ import kotlin.math.max
 class ParticleFactory {
 
     private val particles: MutableList<Particle> = mutableListOf()
-    var ttl = 300
+    var ttl = 3000
         set(value) {
             if (value in 10..1000) {
                 field = value
@@ -27,5 +27,36 @@ class ParticleFactory {
         }
 
 
+    init {
+        for (i in 1..maxParticles) {
+            val tmp = Particle()
+            //TODO set initial particle parameters
+            particles.add(tmp)
+        }
 
+        val mainHandler = Handler(Looper.getMainLooper())
+
+        mainHandler.post(object : Runnable {
+            override fun run() {
+                refreshStat()
+                mainHandler.postDelayed(this, refreshRate)
+            }
+        })
+    }
+
+    private fun refreshStat() {
+        val currentTime = System.currentTimeMillis()
+        for (p in particles.size - 1 downTo 0) {
+            if (currentTime > particles[p].creationTime + ttl) {
+                particles.removeAt(p)
+                continue
+            }
+            particles[p].opacity = 1 - (currentTime - particles[p].creationTime) / ttl.toFloat()
+        }
+        for (i in 0 until maxParticles - particles.size) {
+            val tmp = Particle()
+            //TODO set initial particle parameters
+            particles.add(tmp)
+        }
+    }
 }
