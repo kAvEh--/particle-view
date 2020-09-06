38fd6b6d28e5e5fd44ca9dac2cac38bf1e9997c4
kaveh <kaveh.fazaeli@gmail.com>
2020-09-06 10:35:42 +0430

    Add sample view to main activity

--- particle/src/main/java/com/kaveh/particle/ParticleProgressBar.kt
+++ particle/src/main/java/com/kaveh/particle/ParticleProgressBar.kt
@@ -1,6 +1,20 @@
 package com.kaveh.particle
 
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.view.View
 
-class ParticleProgressBar {
 
+class ParticleProgressBar @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+
+    }
 }
